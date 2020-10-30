f53fbdce4e81b654cc87e86c9895cbb8d1a19931
Karthik-Raman-api <73459326+Karthik-Raman-api@users.noreply.github.com>
2020-10-30 13:21:06 +0530

    Add files via upload

---
+++ Demodt.java
@@ -0,0 +1,22 @@
+public class Demodt
+{
+  private int day,month,year;
+  MyDate()
+  {
+   day=20;month=10;year=2020;
+  }
+   int getDay(){return day;}
+   void setDay(int d){day=d;}
+   void display()
+     {
+        System.out.println(day+"/"+month+"/"+year);
+      }
+   
+   public static void main(String args[])
+   {
+    Demodt d=new Demodt();
+    d.display();
+
+   }
+}
+
